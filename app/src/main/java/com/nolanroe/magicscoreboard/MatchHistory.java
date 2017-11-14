4611d981af6821827f264128753663ef911274a5
danolanater <danolanater@gmail.com>
2017-11-13 18:22:10 -0800

    show match history in shit way

--- app/src/main/java/com/nolanroe/magicscoreboard/MatchHistory.java
+++ app/src/main/java/com/nolanroe/magicscoreboard/MatchHistory.java
@@ -3,8 +3,8 @@ package com.nolanroe.magicscoreboard;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 /**
  * Created by Danolanater on 10/9/2017.
@@ -18,13 +18,15 @@ public class MatchHistory extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.match_history);
         myDb = new DatabaseHelper(this);
+        viewAll();
     }
 
-    public void viewAll(View view) {
+    public void viewAll() {
         Cursor res = myDb.getAllData();
-        if(res.getCount() == 0)
+        if(res.getCount() == 0) {
+            showMessage("Error", "No data found");
             return;
-
+        }
         StringBuffer buffer = new StringBuffer();
         while (res.moveToNext()) {
             buffer.append("Id :" + res.getString(0) + "\n");
@@ -32,5 +34,16 @@ public class MatchHistory extends AppCompatActivity {
             buffer.append("Match Type :" + res.getString(2) + "\n");
             buffer.append("Result :" + res.getString(3) + "\n\n");
         }
+
+        showMessage("Data",buffer.toString());
+    }
+
+    public void showMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
+
